f2bb5eb8ebc7c701be64dc0fd2f91f6669032db7
JK Projects <112632022+jkprojects777@users.noreply.github.com>
2023-02-14 14:17:28 +0100

    Update D3-Gauge-animated-template.js

--- D3-Gauge-animated-template.js
+++ D3-Gauge-animated-template.js
@@ -1,18 +1,21 @@
-
+// Définition des couleurs
 const backgroundColor = "#1a2d52";
 const barColor = "#334465";
 const textColor = "#ffffff";
 
+// Sélection de l'élément conteneur de la jauge et définition des options de responsivité
 const container = d3.select('#gauge-container');
 const width = container.node().getBoundingClientRect().width;
 const height = width;
 const radius = Math.min(width, height) / 2;
 const fontSize = radius * 0.3;
 
+// Définition de l'échelle des angles
 const angleScale = d3.scaleLinear()
   .domain([0, 100])
   .range([0, Math.PI * 2]);
 
+// Création de l'arc de fond de la jauge
 const backgroundArc = d3.arc()
   .innerRadius(radius * 0.7)
   .outerRadius(radius * 0.9)
@@ -20,6 +23,7 @@ const backgroundArc = d3.arc()
   .cornerRadius(radius * 0.1)
   .endAngle(angleScale(100));
 
+// Création de l'élément SVG et ajout de l'arc de fond
 const svg = container.append('svg')
   .attr('width', width)
   .attr('height', height);
@@ -29,17 +33,20 @@ svg.append('path')
   .attr('fill', backgroundColor)
   .attr('transform', `translate(${width/2}, ${height/2})`);
 
+// Création de l'arc de la jauge
 const arc = d3.arc()
   .innerRadius(radius * 0.7)
   .outerRadius(radius * 0.9)
   .startAngle(0)
   .cornerRadius(radius * 0.1);
 
+// Création de l'élément représentant la jauge
 const bar = svg.append('path')
   .datum({endAngle: 0})
   .attr('fill', barColor)
   .attr('transform', `translate(${width/2}, ${height/2})`);
 
+// Création de l'élément texte affichant la valeur de la jauge
 const text = svg.append('text')
   .attr('text-anchor', 'middle')
   .attr('dy', '0.35em')
@@ -47,14 +54,17 @@ const text = svg.append('text')
   .style('font-size', fontSize + 'px')
   .attr('transform', `translate(${width/2}, ${height/2})`);
 
+// Initialisation de la variable indiquant si l'animation de la jauge est en cours
 let isAnimated = false;
 
+// Options de l'observeur d'intersection
 const options = {
   root: null,
   rootMargin: '0px',
   threshold: 0.5
 };
 
+// Création de l'observeur d'intersection
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting && !isAnimated) {
@@ -66,8 +76,10 @@ const observer = new IntersectionObserver((entries, observer) => {
   });
 }, options);
 
+// Observation de l'élément conteneur de la jauge
 observer.observe(container.node());
 
+// Fonction pour dessiner la jauge animée
 const drawGaugeAnimated = (value) => {
   const interpolate = d3.interpolate(0, angleScale(value));
   bar.transition()
