cab08913374852ab2faf47f1bde8d8af68fa5f6c
JK Projects <112632022+jkprojects777@users.noreply.github.com>
2023-02-14 12:09:05 +0100

    Update Tillada-gauge.js

--- Tillada-gauge.js
+++ Tillada-gauge.js
@@ -1,76 +1,54 @@
-const backgroundColor = "#1a2d52"; // Couleur de fond de la jauge
-const barColor = "#334465"; // Couleur des barres de la jauge
-const textColor = "#ffffff"; // Couleur du texte central
 
-// Sélectionne le conteneur
+const backgroundColor = "#1a2d52";
+const barColor = "#334465";
+const textColor = "#ffffff";
+
 const container = d3.select('#gauge-container');
-
-// Définit les dimensions de la jauge
 const width = container.node().getBoundingClientRect().width;
 const height = width;
 const radius = Math.min(width, height) / 2;
+const fontSize = radius * 0.3;
 
-// Définit l'angle de la jauge
 const angleScale = d3.scaleLinear()
   .domain([0, 100])
   .range([0, Math.PI * 2]);
 
-const drawGaugeAnimated = (value) => {
-  // Crée le canevas SVG
-  const svg = container.append('svg')
-    .attr('width', width)
-    .attr('height', height);
+const backgroundArc = d3.arc()
+  .innerRadius(radius * 0.7)
+  .outerRadius(radius * 0.9)
+  .startAngle(0)
+  .cornerRadius(radius * 0.1)
+  .endAngle(angleScale(100));
 
-  // Crée l'arc qui représente la jauge de fond
-  const backgroundArc = d3.arc()
-    .innerRadius(radius * 0.7)
-    .outerRadius(radius * 0.9)
-    .startAngle(0)
-    .cornerRadius(radius * 0.1)
-    .endAngle(angleScale(100));
+const svg = container.append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
-  // Dessine la barre de fond
-  svg.append('path')
-    .attr('d', backgroundArc)
-    .attr('fill', backgroundColor)
-    .attr('transform', `translate(${width/2}, ${height/2})`);
+svg.append('path')
+  .attr('d', backgroundArc)
+  .attr('fill', backgroundColor)
+  .attr('transform', `translate(${width/2}, ${height/2})`);
 
-  // Crée l'arc qui représente la jauge de valeur
-  const arc = d3.arc()
-    .innerRadius(radius * 0.7)
-    .outerRadius(radius * 0.9)
-    .startAngle(0)
-    .cornerRadius(radius * 0.1);
+const arc = d3.arc()
+  .innerRadius(radius * 0.7)
+  .outerRadius(radius * 0.9)
+  .startAngle(0)
+  .cornerRadius(radius * 0.1);
 
-  // Dessine la barre de valeur avec une animation
-  const bar = svg.append('path')
-    .datum({endAngle: 0})
-    .attr('fill', barColor)
-    .attr('transform', `translate(${width/2}, ${height/2})`)
-    .transition()
-    .duration(1500) // Durée de l'animation
-    .attrTween('d', (d) => {
-      const interpolate = d3.interpolate(d.endAngle, angleScale(value));
-      return (t) => {
-        d.endAngle = interpolate(t);
-        return arc(d);
-      };
-    });
+const bar = svg.append('path')
+  .datum({endAngle: 0})
+  .attr('fill', barColor)
+  .attr('transform', `translate(${width/2}, ${height/2})`);
 
-  // Calcule la taille du texte en fonction de la moitié du rayon de la jauge
-  const fontSize = radius * 0.3;
+const text = svg.append('text')
+  .attr('text-anchor', 'middle')
+  .attr('dy', '0.35em')
+  .attr('fill', textColor)
+  .style('font-size', fontSize + 'px')
+  .attr('transform', `translate(${width/2}, ${height/2})`);
 
-  // Ajoute du texte pour afficher la valeur de la jauge avec un symbole de pourcentage et une taille de police personnalisée
-  const text = svg.append('text')
-    .attr('text-anchor', 'middle')
-    .attr('dy', '0.35em')
-    .attr('fill', textColor)
-    .style('font-size', fontSize + 'px') // Définit la taille de police personnalisée
-    .text(value + '%')
-    .attr('transform', `translate(${width/2}, ${height/2})`);
-};
+let isAnimated = false;
 
-// Utilisez la fonction IntersectionObserver pour déclencher l'animation lorsque l'élément est visible
 const options = {
   root: null,
   rootMargin: '0px',
@@ -79,12 +57,35 @@ const options = {
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      drawGaugeAnimated(50); // Mettez la valeur que vous souhaitez ici
-      observer.unobserve(entry.target);
-      observer.observe(entry.target); // Réactive l'observer pour que l'animation recommence
+    if (entry.isIntersecting && !isAnimated) {
+      drawGaugeAnimated(50);
+      isAnimated = true;
+    } else if (!entry.isIntersecting) {
+      isAnimated = false;
     }
   });
 }, options);
 
 observer.observe(container.node());
+
+const drawGaugeAnimated = (value) => {
+  const interpolate = d3.interpolate(0, angleScale(value));
+  bar.transition()
+    .duration(1500)
+    .attrTween('d', d => {
+      return t => {
+        d.endAngle = interpolate(t);
+        return arc(d);
+      };
+    });
+
+  text.transition()
+    .duration(1500)
+    .tween('text', () => {
+      const interpolate = d3.interpolate(0, value);
+      return t => {
+        text.text(Math.round(interpolate(t)) + '%');
+      };
+    });
+};
+
