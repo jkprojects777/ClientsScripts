f8edd57a315d45e276c0e8bfb5c4b79fd7fb90de
JK Projects <112632022+jkprojects777@users.noreply.github.com>
2023-02-21 00:06:53 +0100

    Update and rename qs-swipers8.js to qs-swipers9.js

--- qs-swipers9.js
+++ qs-swipers9.js
@@ -1,14 +1,14 @@
 const swiperOptions={
-  mousewheel:!1,
-  autoHeight:!1,
-  forceToAxis:!1,
-  loop:!0,
+  mousewheel:1,
+  autoHeight:1,
+  forceToAxis:1,
+  loop:0,
   speed:1200,
-  freeMode:!1,
-  centeredSlides:!0,
-  centeredSlidesBounds:!0,
-  lazy:!1,
-  keyboard:{enabled:!0},
+  freeMode:1,
+  centeredSlides:0,
+  centeredSlidesBounds:0,
+  lazy:1,
+  keyboard:{enabled:0},
   breakpoints:{
     0:{slidesPerView:1.2,spaceBetween:10},
     479:{slidesPerView:2,spaceBetween:20},
@@ -18,7 +18,7 @@ const swiperOptions={
   }
 };
 
-[".cat1-cards",".cat2-cards",".cat3-cards",".cat4-cards",".cat5-cards",".cat6-cards",".cat7-cards",".cat9-cards"]
+[".cat1-cards",".cat2-cards",".cat3-cards",".cat4-cards",".cat5-cards",".cat6-cards",".cat7-cards"]
   .forEach(e=>{new Swiper(e,{
     ...swiperOptions,navigation:{
       nextEl:`#${e.replace(".","")}-right`,
@@ -36,6 +36,14 @@ const testiswiper=new Swiper(".cat8-cards",{
     delay:3e3,
     pauseOnMouseEnter:!0
   }
+});
+
+const jobswiper=new Swiper(".cat9-cards",{
+  ...swiperOptions,navigation:{
+    nextEl:"#cat9-cards-right",
+    prevEl:"#cat9-cards-left"
+  },
+  loop:1  
 });
 
 $('.w-dyn-empty').parents('.swiper-comp.jobs').each(function(){ $(this).hide()})
