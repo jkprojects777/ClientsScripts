0624bd6d23884de3239753b3170c38b7f6d81ae4
JK Projects <112632022+jkprojects777@users.noreply.github.com>
2024-03-12 17:20:26 +0100

    Update and rename gauge-tld3.js to gauge-tld4.js

--- gauge-tld4.js
+++ gauge-tld4.js
@@ -1,43 +1,36 @@
 function drawGauge(containerSelector, value) {
   const backgroundColor = "rgba(255, 255, 255, 0.07)";
-  const textColor = "#6c6efe";
+  const textColor = "url(#textGradient)";
 
   const container = d3.select(containerSelector);
   const width = container.node().getBoundingClientRect().width;
   const height = width;
   const radius = Math.min(width, height) / 2;
   const fontSize = radius * 0.3;
-
   const angleScale = d3.scaleLinear()
     .domain([0, 100])
     .range([0, Math.PI * 2]);
-
   const backgroundArc = d3.arc()
     .innerRadius(radius * 0.7)
     .outerRadius(radius * 0.9)
     .startAngle(0)
     .cornerRadius(radius * 0.1)
     .endAngle(angleScale(100));
-
   const svg = container.append('svg')
     .attr('width', width)
     .attr('height', height);
-
   svg.append('path')
     .attr('d', backgroundArc)
     .attr('fill', backgroundColor)
     .attr('transform', `translate(${width/2}, ${height/2})`);
-
   const arc = d3.arc()
     .innerRadius(radius * 0.7)
     .outerRadius(radius * 0.9)
     .startAngle(0)
     .cornerRadius(radius * 0.1);
-
   const colorScale = d3.scaleLinear()
     .domain([0, 1])
     .range(['#272265', '#8a3574', '#f7ac39']);
-
   const gradient = svg.append("defs")
     .append("linearGradient")
     .attr("id", "gradient")
@@ -45,23 +38,19 @@ function drawGauge(containerSelector, value) {
     .attr("y1", "0%")
     .attr("x2", "100%")
     .attr("y2", "0%");
-
   const gradientStops = colorScale.range().map((color, index) => ({
     offset: index / (colorScale.range().length - 1),
     color: color
   }));
-
   gradient.selectAll("stop")
     .data(gradientStops)
     .enter().append("stop")
     .attr("offset", d => `${d.offset * 100}%`)
     .attr("stop-color", d => d.color);
-
   const bar = svg.append('path')
     .datum({endAngle: 0})
-    .attr('fill', '#6c6efe')
+    .attr('fill', 'url(#gradient)')
     .attr('transform', `translate(${width/2}, ${height/2})`);
-
   const textGradient = svg.append("defs")
     .append("linearGradient")
     .attr("id", "textGradient")
@@ -69,28 +58,23 @@ function drawGauge(containerSelector, value) {
     .attr("y1", "0%")
     .attr("x2", "100%")
     .attr("y2", "0%");
-
   textGradient.selectAll("stop")
     .data(gradientStops)
     .enter().append("stop")
     .attr("offset", d => `${d.offset * 100}%`)
     .attr("stop-color", d => d.color);
-
   const text = svg.append('text')
   .attr('text-anchor', 'middle')
 .attr('dy', '0.35em')
 .attr('fill', textColor)
 .style('font-size', fontSize + 'px')
 .attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
-
 let isAnimated = false;
-
 const options = {
 root: null,
 rootMargin: '0px',
 threshold: 0.5
 };
-
 const observer = new IntersectionObserver((entries, observer) => {
 entries.forEach(entry => {
 if (entry.isIntersecting && !isAnimated) {
@@ -101,9 +85,7 @@ isAnimated = false;
 }
 });
 }, options);
-
 observer.observe(container.node());
-
 const drawGaugeAnimated = (value) => {
 const interpolate = d3.interpolate(0, angleScale(value));
 bar.transition()
@@ -125,11 +107,9 @@ text.transition()
   });
   };
 };
-
 setTimeout(function() {
 drawGauge('#gauge-container-1', 96);
 drawGauge('#gauge-container-2', 85);
 drawGauge('#gauge-container-3', 100);
 drawGauge('#gauge-container-4', 100);
 }, 0);
-  
